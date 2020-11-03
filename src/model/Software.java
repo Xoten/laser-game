687ea83e94e09544551bdea607e8a42b9b856e57
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2020-11-02 23:02:08 -0500

    little changes on shoot method

--- src/model/Software.java
+++ src/model/Software.java
@@ -10,7 +10,6 @@ public class Software {
 
 	}
 
-
 	public Node getExitNode() {
 		return exitNode;
 	}
@@ -66,7 +65,6 @@ public class Software {
 				if(ToLocateStart.getMirror().equals("/")) {
 
 					InitialD = "left";
-
 				}else {
 
 					InitialD = "right";
@@ -102,46 +100,34 @@ public class Software {
 				if(ToLocateStart.getMirror().equals("/")) {
 
 					InitialD = "up";
-
 				}else {
 
 					InitialD = "down";
 				}
-
 
 			}else {
 
 				InitialD = "right";
 			}
 
-
 		}else if(ToLocateStart.getNext() == null) {
 
 			if(!ToLocateStart.getMirror().equals("")) {
 
 				if(ToLocateStart.getMirror().equals("/")) {
-
 					InitialD = "down";
-
 				}else {
-
 					InitialD = "up";
 				}
 
-
 			}else {
-
 				InitialD = "left";
 			}
-
-
 		}
-
 		return InitialD;
 	}
 
 	public String toDetermineInitialDirectionatCorner(String orientation, Node ToLocateStart) {
-
 
 		String Direction = "";
 
@@ -153,7 +139,6 @@ public class Software {
 				if(!ToLocateStart.getMirror().equals("")) {
 
 					if(ToLocateStart.getMirror().equals("/")) {
-
 
 						Direction = "down";
 					}else {
@@ -171,7 +156,6 @@ public class Software {
 
 					if(ToLocateStart.getMirror().equals("/")){
 
-
 						Direction = "up";
 					}else {
 
@@ -180,10 +164,8 @@ public class Software {
 
 				}else {
 
-
 					Direction = "right";
 				}
-
 
 
 			}if(orientation.equalsIgnoreCase("V")){
@@ -212,10 +194,8 @@ public class Software {
 
 					if(ToLocateStart.getMirror().equals("/")) {
 
-
 						Direction = "down";
 					}else {
-
 						Direction = "up";
 					}
 				}else {
@@ -232,17 +212,13 @@ public class Software {
 
 						Direction = "up";
 					}else {
-
 						Direction = "down";
 					}
 
 				}else {
 
-
 					Direction = "right";
 				}
-
-
 
 			}if(orientation.equalsIgnoreCase("V")){
 
@@ -262,239 +238,237 @@ public class Software {
 			}
 		}
 
-	
+		return Direction;
 
-	return Direction;
-
-}
+	}
 
 
 
 
-public Node Shoot(Node ShootEnd, String initialTrayect) {
+	public Node Shoot(Node ShootEnd, String initialTrayect) {
 
-	if(initialTrayect.equalsIgnoreCase("down")) {
+		if(initialTrayect.equalsIgnoreCase("down")) {
 
-		if(ShootEnd.getDown() != null) {
+			if(ShootEnd.getDown() != null) {
 
-			if(ShootEnd.getDown().getMirror().equals("\\")) {
-
-
-				initialTrayect = "right";
-				ShootEnd = ShootEnd.getDown();
-				return Shoot(ShootEnd, initialTrayect);
-			}else if(ShootEnd.getDown().getMirror().equals("/")) {
-
-				initialTrayect = "left";
-				ShootEnd = ShootEnd.getDown();
-				return Shoot(ShootEnd, initialTrayect);
-			}else {
+				if(ShootEnd.getDown().getMirror().equals("\\")) {
 
 
+					initialTrayect = "right";
+					ShootEnd = ShootEnd.getDown();
+					return Shoot(ShootEnd, initialTrayect);
+				}else if(ShootEnd.getDown().getMirror().equals("/")) {
 
-				ShootEnd = ShootEnd.getDown();
-				return Shoot(ShootEnd, initialTrayect);
+					initialTrayect = "left";
+					ShootEnd = ShootEnd.getDown();
+					return Shoot(ShootEnd, initialTrayect);
+				}else {
+
+
+
+					ShootEnd = ShootEnd.getDown();
+					return Shoot(ShootEnd, initialTrayect);
+				}
+
 			}
+
+			return ShootEnd;
 
 		}
 
+		if(initialTrayect.equalsIgnoreCase("up")) {
+
+			if(ShootEnd.getUp() != null) {
+
+				if(ShootEnd.getUp().getMirror().equals("\\")) {
+
+
+					initialTrayect = "left";
+					ShootEnd = ShootEnd.getUp();
+					return  Shoot(ShootEnd, initialTrayect);
+				}else if(ShootEnd.getUp().getMirror().equals("/")) {
+
+					initialTrayect = "right";
+					ShootEnd = ShootEnd.getUp();
+					return Shoot(ShootEnd, initialTrayect);
+				}else {
+
+					ShootEnd = ShootEnd.getUp();
+					return Shoot(ShootEnd, initialTrayect);
+
+				}
+			}
+
+			return ShootEnd;
+
+		}
+		if(initialTrayect.equalsIgnoreCase("Right")) {
+
+			if(ShootEnd.getNext() != null) {
+
+				if(ShootEnd.getNext().getMirror().equals("\\")) {
+
+
+					initialTrayect = "down";
+					ShootEnd = ShootEnd.getNext();
+					return Shoot(ShootEnd, initialTrayect);
+				}else if(ShootEnd.getNext().getMirror().equals("/")) {
+
+					initialTrayect = "up";
+					ShootEnd = ShootEnd.getNext();
+					return Shoot(ShootEnd, initialTrayect);
+				}else {
+
+					ShootEnd = ShootEnd.getNext();
+					return Shoot(ShootEnd, initialTrayect);
+
+				}
+
+			}
+
+			return ShootEnd;
+
+		}
+		if(initialTrayect.equalsIgnoreCase("Left")) {
+
+			if(ShootEnd.getPrev() != null) {
+
+				if(ShootEnd.getPrev().getMirror().equals("\\")) {
+
+
+					initialTrayect = "up";
+					ShootEnd = ShootEnd.getPrev();
+					return Shoot(ShootEnd, initialTrayect);
+				}else if(ShootEnd.getPrev().getMirror().equals("/")) {
+
+					initialTrayect = "down";
+					ShootEnd = ShootEnd.getPrev();
+					return Shoot(ShootEnd, initialTrayect);
+
+
+				}else {
+
+
+					ShootEnd = ShootEnd.getPrev();
+					return Shoot(ShootEnd, initialTrayect);
+
+				}
+			}
+		}
 		return ShootEnd;
-
 	}
 
-	if(initialTrayect.equalsIgnoreCase("up")) {
-
-		if(ShootEnd.getUp() != null) {
-
-			if(ShootEnd.getUp().getMirror().equals("\\")) {
-
-
-				initialTrayect = "left";
-				ShootEnd = ShootEnd.getUp();
-				return  Shoot(ShootEnd, initialTrayect);
-			}else if(ShootEnd.getUp().getMirror().equals("/")) {
-
-				initialTrayect = "right";
-				ShootEnd = ShootEnd.getUp();
-				Shoot(ShootEnd, initialTrayect);
+	public Node toFindPosition(Node currentNode, int toSearchRow, int toSearchColum) {
+		Node toSearch = null;
+		if(currentNode!=null) {
+			if(currentNode.getRow() == toSearchRow && currentNode.getCol() == toSearchColum) {
+				return currentNode;
 			}else {
-
-				ShootEnd = ShootEnd.getUp();
-				return Shoot(ShootEnd, initialTrayect);
-
+				toSearch = goThroughRows(currentNode.getNext(),toSearchRow,toSearchColum);
+			}
+			if(toSearch==null) {
+				toSearch = toFindPosition(currentNode.getDown(),toSearchRow,toSearchColum);
 			}
 		}
-
-		return ShootEnd;
-
+		return toSearch; 
 	}
-	if(initialTrayect.equalsIgnoreCase("Right")) {
 
-		if(ShootEnd.getNext() != null) {
-
-			if(ShootEnd.getNext().getMirror().equals("\\")) {
-
-
-				initialTrayect = "down";
-				ShootEnd = ShootEnd.getNext();
-				return Shoot(ShootEnd, initialTrayect);
-			}else if(ShootEnd.getNext().getMirror().equals("/")) {
-
-				initialTrayect = "up";
-				ShootEnd = ShootEnd.getNext();
-				return Shoot(ShootEnd, initialTrayect);
+	public Node goThroughRows(Node currentN, int toFindRow, int toFindColumn) {
+		Node toSearch = null;
+		if(currentN!=null) {
+			if(currentN.getRow() == toFindRow && currentN.getCol() == toFindColumn) {
+				return currentN;
 			}else {
-
-				ShootEnd = ShootEnd.getNext();
-				return Shoot(ShootEnd, initialTrayect);
-
-			}
-
-		}
-
-		return ShootEnd;
-
-	}
-	if(initialTrayect.equalsIgnoreCase("Left")) {
-
-		if(ShootEnd.getPrev() != null) {
-
-			if(ShootEnd.getPrev().getMirror().equals("\\")) {
-
-
-				initialTrayect = "up";
-				ShootEnd = ShootEnd.getPrev();
-				return Shoot(ShootEnd, initialTrayect);
-			}else if(ShootEnd.getPrev().getMirror().equals("/")) {
-
-				initialTrayect = "down";
-				ShootEnd = ShootEnd.getPrev();
-				return Shoot(ShootEnd, initialTrayect);
-
-
-			}else {
-
-
-				ShootEnd = ShootEnd.getPrev();
-				return Shoot(ShootEnd, initialTrayect);
-
+				toSearch = goThroughRows(currentN.getNext(), toFindRow, toFindColumn);
 			}
 		}
+		return toSearch;
 	}
-	return ShootEnd;
-}
 
-public Node toFindPosition(Node currentNode, int toSearchRow, int toSearchColum) {
-	Node toSearch = null;
-	if(currentNode!=null) {
-		if(currentNode.getRow() == toSearchRow && currentNode.getCol() == toSearchColum) {
-			return currentNode;
-		}else {
-			toSearch = goThroughRows(currentNode.getNext(),toSearchRow,toSearchColum);
-		}
-		if(toSearch==null) {
-			toSearch = toFindPosition(currentNode.getDown(),toSearchRow,toSearchColum);
-		}
-	}
-	return toSearch; 
-}
+	public boolean toLocateMirror(int rowFire, int ColFire, String inclination) {
 
-public Node goThroughRows(Node currentN, int toFindRow, int toFindColumn) {
-	Node toSearch = null;
-	if(currentN!=null) {
-		if(currentN.getRow() == toFindRow && currentN.getCol() == toFindColumn) {
-			return currentN;
-		}else {
-			toSearch = goThroughRows(currentN.getNext(), toFindRow, toFindColumn);
-		}
-	}
-	return toSearch;
-}
+		boolean guessed;
+		String mirrorinc = "";
+		if(inclination.equalsIgnoreCase("L")) {
 
-public boolean toLocateMirror(int rowFire, int ColFire, String inclination) {
-
-	boolean guessed;
-	String mirrorinc = "";
-	if(inclination.equalsIgnoreCase("L")) {
-
-		mirrorinc = "\\";
-
-	}else {
-
-		mirrorinc = "/";
-	}
-	Node toLocate = toFindPosition(LinkedM.getFirst(), rowFire, ColFire);
-
-	if(toLocate.getMirror() != null) {
-
-		if(toLocate.getMirror().equalsIgnoreCase(mirrorinc)){	
-
-			toLocate.setValue(toLocate.getMirror());	
-			guessed = true;
-			toLocate.setVisibility(true);
+			mirrorinc = "\\";
 
 		}else {
-			toLocate.setValue("X");	
+
+			mirrorinc = "/";
+		}
+		Node toLocate = toFindPosition(LinkedM.getFirst(), rowFire, ColFire);
+
+		if(toLocate.getMirror() != null) {
+
+			if(toLocate.getMirror().equalsIgnoreCase(mirrorinc)){	
+
+				toLocate.setValue(toLocate.getMirror());	
+				guessed = true;
+				toLocate.setVisibility(true);
+
+			}else {
+				toLocate.setValue("X");	
+				guessed = false;
+			}
+
+		}else {
+			toLocate.setValue("X");
 			guessed = false;
 		}
 
-	}else {
-		toLocate.setValue("X");
-		guessed = false;
+		return guessed;
+
+
 	}
 
-	return guessed;
+	public void verifyStateAtEnd(Node StartToVerify, Node endToVerify) {
 
+		if(StartToVerify.getVisibility() == true ) {
 
-}
+			StartToVerify.setValue(StartToVerify.getMirror());
 
-public void verifyStateAtEnd(Node StartToVerify, Node endToVerify) {
-
-	if(StartToVerify.getVisibility() == true ) {
-
-		StartToVerify.setValue(StartToVerify.getMirror());
-
-	}else {
-
-		StartToVerify.setValue("");
-
-	} if(endToVerify.getVisibility() == true){
-
-
-		endToVerify.setValue(endToVerify.getMirror());
-
-	}else {
-
-		endToVerify.setValue("");
-	}
-
-}
-
-public void generateRandomMirrors(int mirrorN, int n, int m) {
-	if(mirrorN!=0) {
-		int aleatoryN = (int)(Math.random()*n+1)-1;
-		int aleatoryM = (int)(Math.random()*m+1)-1;
-		Node toSearch = toFindPosition(LinkedM.getFirst(), aleatoryN, aleatoryM);
-		if(toSearch.getMirror().equals("")) {
-			int mirrorRandom = (int)(Math.random()*2+1);
-			if(mirrorRandom==1) {
-				toSearch.setMirror("/");
-			}else if(mirrorRandom==2) {
-				toSearch.setMirror("\\");
-			}
-			generateRandomMirrors(mirrorN-1,n,m);
 		}else {
-			generateRandomMirrors(mirrorN, n, m);
+
+			StartToVerify.setValue("");
+
+		} if(endToVerify.getVisibility() == true){
+
+
+			endToVerify.setValue(endToVerify.getMirror());
+
+		}else {
+
+			endToVerify.setValue("");
+		}
+
+	}
+
+	public void generateRandomMirrors(int mirrorN, int n, int m) {
+		if(mirrorN!=0) {
+			int aleatoryN = (int)(Math.random()*n+1)-1;
+			int aleatoryM = (int)(Math.random()*m+1)-1;
+			Node toSearch = toFindPosition(LinkedM.getFirst(), aleatoryN, aleatoryM);
+			if(toSearch.getMirror().equals("")) {
+				int mirrorRandom = (int)(Math.random()*2+1);
+				if(mirrorRandom==1) {
+					toSearch.setMirror("/");
+				}else if(mirrorRandom==2) {
+					toSearch.setMirror("\\");
+				}
+				generateRandomMirrors(mirrorN-1,n,m);
+			}else {
+				generateRandomMirrors(mirrorN, n, m);
+			}
 		}
 	}
-}
 
-public void toAddLinkedMatrix(int m, int n) {
+	public void toAddLinkedMatrix(int m, int n) {
 
 
-	LinkedM = new LinkedMatrix(m,n);
+		LinkedM = new LinkedMatrix(m,n);
 
-}
+	}
 
 
 
