2fe7dc647f407bea0d421414f400638a5e857bd5
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2020-11-05 16:32:08 -0500

    recursive menu, fixed, now when the user wins, it shows the menu again

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -16,18 +16,18 @@ public class Menu {
 	final static int EXIT_MENU = 3;
 	Software sw = new Software();
 	int mirrorsLeft;
-	
+
 	/**
 	 * This method is the constructor of Menu
 	 */
 	public Menu() {
-		
+
 		this.sw = startGame();
 	}
-	
 
-	
-	
+
+
+
 	/**
 	 * This method shows the Menu
 	 * @return the menu
@@ -42,7 +42,7 @@ public class Menu {
 		menu += "1. Start a new game\n";
 		menu += "2. Show Scores\n";
 		menu += "3. Exit Game\n";
-		
+
 		return menu;
 	}
 	private void executeOperation(int option) {
@@ -53,7 +53,7 @@ public class Menu {
 			playGame();
 			break;
 		case 2:
-            showPositions();
+			showPositions();
 			break;
 		case 3:
 			exitProgram();
@@ -79,13 +79,13 @@ public class Menu {
 		int k = Integer.parseInt(parts[3]);
 		mirrorsLeft = k;
 		sw.toAddLinkedMatrix(m, n);
-		
-		
-		
+
+
+
 		if(k <= m*n) {
 			sw.generateRandomMirrors(k, n, m);
 			shootOption(false, m, n, nickName, k);
-			
+
 		} else {
 			System.out.println("Mirrors must be minors than the matrix size");
 		}
@@ -93,140 +93,136 @@ public class Menu {
 
 	public void shootOption(boolean stop,int m, int n, String nickName, int k) {
 		stop = false;
-	
-			
-		
-		if(mirrorsLeft == 0) {
-			  
-			System.out.println("You win, you are pro" + " Score: " + sw.getScore() + " total tries: " + sw.getTries());
+
+
+
+		 if(mirrorsLeft == 0) {
+
+			System.out.println("You win, you are pro player" + " Score: " + sw.getScore() + " total tries: " + sw.getTries());
 			Player newUser = new Player(nickName, sw.getScore());
 			sw.addPlayer(newUser);
-			
-			 stop = true;
-			
-			
-			 recursiveMenu(1, getMenu());
-			  
-			}else {
-		    System.out.println(sw.getLinkedM());
+
+			stop = true;
+
+		   }else {
+			System.out.println(sw.getLinkedM());
 			System.out.println(nickName+": "+mirrorsLeft+" mirrors remaining" + " current Score: " +sw.getScore() + " current tries " +sw.getTries());
 			System.out.println("Type menu to exit, L to locate or coordinate to fire");
-			
-			}
-		
-		String fire = sc.nextLine();
-		String [] fParts = fire.split("");
-		if(fire.equalsIgnoreCase("Menu") || stop == true ) {
-			
-			
-			stop = true;
-			Player newUser = new Player(nickName, sw.getScore());
-			sw.addPlayer(newUser);
-			System.out.println(nickName+": "+mirrorsLeft+"mirrors remaining" +" Score: "+ sw.getScore() + " total tries: " +sw.getTries());
-			
-			
 
-		} else if(fParts[0].equalsIgnoreCase("L")){
-			boolean mirrorFound = false;
-			int rowFire = Integer.parseInt(fParts[1]);
-			char colFireChar = fParts[2].charAt(0);
-			int colFire = colFireChar;
-			int colToFire = (char)(colFire-'A');
-			String directorMirror = (fParts[3]);
-			if(sw.toLocateMirror(rowFire-1, colToFire, directorMirror) == false) {
-				System.out.println(sw.getLinkedM());
-				sw.toFindPosition(sw.getLinkedM().getFirst(), rowFire-1, colToFire).setValue("");
-			} else {
-				mirrorFound = true;
-				
-			}
-			if(mirrorFound == true) {
-				mirrorsLeft--;
-				System.out.println(sw.getLinkedM());
-			}
-			shootOption(stop, m, n,  nickName, k);
+			String fire = sc.nextLine();
+			String [] fParts = fire.split("");
+			if(fire.equalsIgnoreCase("Menu") || stop == true ) {
 
-		} else {
-			int rowFire = Integer.parseInt(fParts[0]);
-			char colFireChar = fParts[1].charAt(0);
-			int colFire = colFireChar;
-			int colToFire = (char)(colFire-'A');
-			String directorFire = "";
-			if(fParts.length == 3) {
-				directorFire = (fParts[2]);
-			}
-			if(rowFire-1 < m && colToFire < n) {
-				if(sw.toShoot(rowFire-1, colToFire, directorFire) == false) {
-					System.out.println("Fire can not be executed");
-				} else {
-					System.out.println("Fired!!");
+
+				stop = true;
+				Player newUser = new Player(nickName, sw.getScore());
+				sw.addPlayer(newUser);
+				System.out.println(nickName+": "+mirrorsLeft+"mirrors remaining" +" Score: "+ sw.getScore() + " total tries: " +sw.getTries());
+
+
+
+			} else if(fParts[0].equalsIgnoreCase("L")){
+				boolean mirrorFound = false;
+				int rowFire = Integer.parseInt(fParts[1]);
+				char colFireChar = fParts[2].charAt(0);
+				int colFire = colFireChar;
+				int colToFire = (char)(colFire-'A');
+				String directorMirror = (fParts[3]);
+				if(sw.toLocateMirror(rowFire-1, colToFire, directorMirror) == false) {
 					System.out.println(sw.getLinkedM());
-					sw.verifyStateAtEnd(sw.toFindPosition(sw.getLinkedM().getFirst(), rowFire-1, colToFire), sw.getExitNode());
+					sw.toFindPosition(sw.getLinkedM().getFirst(), rowFire-1, colToFire).setValue("");
+				} else {
+					mirrorFound = true;
+
 				}
+				if(mirrorFound == true) {
+					mirrorsLeft--;
+					System.out.println(sw.getLinkedM());
+				}
+				shootOption(stop, m, n,  nickName, k);
+
 			} else {
-			
-				System.out.println("Coordinates out of matrix!!");
+				int rowFire = Integer.parseInt(fParts[0]);
+				char colFireChar = fParts[1].charAt(0);
+				int colFire = colFireChar;
+				int colToFire = (char)(colFire-'A');
+				String directorFire = "";
+				if(fParts.length == 3) {
+					directorFire = (fParts[2]);
+				}
+				if(rowFire-1 < m && colToFire < n) {
+					if(sw.toShoot(rowFire-1, colToFire, directorFire) == false) {
+						System.out.println("Fire can not be executed");
+					} else {
+						System.out.println("Fired!!");
+						System.out.println(sw.getLinkedM());
+						sw.verifyStateAtEnd(sw.toFindPosition(sw.getLinkedM().getFirst(), rowFire-1, colToFire), sw.getExitNode());
+					}
+				} else {
+
+					System.out.println("Coordinates out of matrix!!");
+				}
+
+				shootOption(stop, m, n, nickName, k);
 			}
-	
-			shootOption(stop, m, n, nickName, k);
 		}
 	}
-	
+
 	/**
 	 * This method displays the menu depending on user choice
 	 * @param option is the option the user selects
 	 * @param menu 
 	 */
 	public void recursiveMenu(int option, String menu) {
-        if(option == EXIT_MENU) {
-           
-        } else {
-            System.out.println(menu);
-            option = readOption();
-            executeOperation(option);
-            recursiveMenu(option, menu);
-        }
-    }
-	
+		if(option == EXIT_MENU) {
+
+		} else {
+			System.out.println(menu);
+			option = readOption();
+			executeOperation(option);
+			recursiveMenu(option, menu);
+		}
+	}
+
 	public void startMenu() {
-        String menu = getMenu();
-        int option = 0;
-        recursiveMenu(option, menu);
-    }
-	
+		String menu = getMenu();
+		int option = 0;
+		recursiveMenu(option, menu);
+	}
+
 	private void exitProgram() {
-		
+
 		sc.close();
 		System.out.println("Good Bye");
-		
-		
+
+
 	}
-	
+
 	public Software startGame() {
-		
-		
+
+
 		sc = new Scanner(System.in);
 		return sw;
 	}
-	
+
 	public int readOption() {
-		
+
 		int option;
-		 option = Integer.parseInt(sc.nextLine());
-		
+		option = Integer.parseInt(sc.nextLine());
+
 		return option;
 	}
-	
+
 	public void showPositions() {
 		System.out.println("\n"+sw.showScore());
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
 }
